574c7fc259bc20e6f6b7def9db74d63e28d88558
Ravin Io <rio@onestaffmedical.com>
2022-12-14 14:22:03 -0600

    added enviroment to master

--- src/context/client.js
+++ src/context/client.js
@@ -2,5 +2,6 @@ import * as contentful from "contentful"
 
 export const client = contentful.createClient({
     space: process.env.REACT_APP_SPACE_ID,
+    environment: "master",
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
