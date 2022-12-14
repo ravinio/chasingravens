db12661f6ae6d31976bd6324e9c4ccde75c701c4
Ravin Io <rio@onestaffmedical.com>
2022-12-14 14:27:08 -0600

    change .env.dev to what I jsut made on contentfu l to attach to vercel

--- src/context/client.js
+++ src/context/client.js
@@ -1,7 +1,6 @@
 import * as contentful from "contentful"
 
 export const client = contentful.createClient({
-    space: process.env.REACT_APP_SPACE_ID,
-    environment: "master",
-    accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
 })
